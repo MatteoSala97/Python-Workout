fb6dd96065766bd8b462f56d5094bc0b2a69be8b
NetItUp-MatteoSala <matteo.sala@netitup.it>
2024-12-16 15:10:45 +0100

    ex-15

---
+++ 15-rainfall/rainfall.py
@@ -0,0 +1,16 @@
+rain = {}
+while True:
+    question = print("Input a city and tell me how much rain has fallen. I'll keep track of it.\n")
+    city = input('Input the name of the city: ')
+    if not city:
+        break
+    try:
+        mm_rain = input('Input the amount of rain in mm: ')
+    except ValueError:
+        print("You stoopid or what?!")
+        continue
+        
+    rain[city]= rain.get(city, 0) + int(mm_rain)
+    
+for c, r in rain.items():
+        print(f"{c}: {r} mm of rain.")
